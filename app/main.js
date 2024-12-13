c0283a44a5fa79daec4b741d074215842818a0f9
Mateusz Kowalski <towermanxxl@gmail.com>
2024-12-13 16:38:18 +0100

    added swap feature

--- app/main.js
+++ app/main.js
@@ -1,1 +1,16 @@
 
+function swap(){
+    var wordBoxes = document.getElementsByClassName("wordBox")
+
+    for (wordBox of wordBoxes){
+
+        var mainWord = wordBox.getElementsByClassName("mainWord")[0]
+        var otherWord = wordBox.getElementsByClassName("otherWord")[0]
+
+        var mainContent = mainWord.innerHTML
+        mainWord.innerHTML = otherWord.innerHTML
+        otherWord.innerHTML = mainContent
+    }
+}
+
+document.getElementById("swapButton").addEventListener("click",swap)
