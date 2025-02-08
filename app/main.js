1780ea59f704aa872fc1567b5bd15f01d1d67edd
Mateusz Kowalski <towermanxxl@gmail.com>
2025-02-08 11:11:27 +0100

    added mode button

--- app/main.js
+++ app/main.js
@@ -18,6 +18,23 @@ function insertElements(jsonData){
         i+= 1
     }
 }
+function  addWordElementHoverEffect(wordElement){
+    wordElement.addEventListener("mouseover",(event)=>{
+        let mode = document.getElementById("learnModeButton").getAttribute("data-mode")
+        if (mode != "test"){
+            return
+        }
+        event.currentTarget.style.color = "rgb(136, 136, 136)"
+    })
+    
+    wordElement.addEventListener("mouseout",(event)=>{
+        let mode = document.getElementById("learnModeButton").getAttribute("data-mode")
+        if (mode != "test"){
+            return
+        }
+        event.currentTarget.style.color = "#1e1e1e"
+    })
+}
 
 function constructWordBox(mainWord,otherWord,id){
     var wordBox = document.createElement("div")
@@ -31,6 +48,8 @@ function constructWordBox(mainWord,otherWord,id){
     var otherWordElement = document.createElement("div")
     otherWordElement.classList.add("word","otherWord")
     otherWordElement.innerHTML = otherWord
+    addWordElementHoverEffect(otherWordElement)
+    
 
     wordBox.append(mainWordElement,otherWordElement)
 
@@ -63,9 +82,33 @@ function handleFile(event){
     reader.readAsText(file)
 
 }
+function learnMode(event){
+    let words = document.getElementsByClassName("otherWord")
+    let mode = event.currentTarget.getAttribute("data-mode")
+    if (mode == "test"){
+        event.currentTarget.setAttribute("data-mode","learn")
+    }
+    else if (mode == "learn"){
+        event.currentTarget.setAttribute("data-mode","test")
+    }
+    for (let word of words){
+        if (event.currentTarget.getAttribute("data-mode") == "test"){
+            word.style.color = "#1e1e1e"
+            continue
+        }
+        word.style.color = "#888"
+        
+             
+        
+    }
+    
+
+}
 
 document.getElementById("swapButton").addEventListener("click",swap)
 document.getElementById("fileInput").addEventListener('change', handleFile)
+document.getElementById("learnModeButton").addEventListener("click",learnMode)
+
 
 
         
